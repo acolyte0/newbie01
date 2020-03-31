09120ee8d1bd93c3c4e4d8cac23c270fc8ed809b
John Rhee <funkyx@nate.com>
2020-03-31 09:46:21 +0900

    js...

--- js/promises.js
+++ js/promises.js
@@ -3,8 +3,36 @@ function f1(v, s, f) {
         v += '- function 1 -';
         s(v);
     } else {
-        f('value invalid');
+        f('value of f1 invalid');
     }
 }
 
-f1('', (v1)=>{console.log(v1)}, (err)=>console.log(err));
+function f2(v, s, f) {
+    if(v) {
+        v += '- function 2 -';
+        s(v);
+    } else {
+        f('value of v2 invalid');
+    }
+}
+
+function f3(v, s, f) {
+    if(v) {
+        v += '- function 3 -';
+        s(v);
+    } else {
+        f('value of v3 invalid');
+    }
+}
+
+f1(
+    'string',
+    (whatever)=>{
+        f2(whatever,
+            (whatever1)=>{
+                console.log(whatever1)
+            },
+        (anyValue)=>console.log(anyValue))
+    },
+    (anything)=>console.log(anything)
+);
