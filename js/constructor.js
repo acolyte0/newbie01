52b30237b1708c6e104d4c8c8b206438aadb885b
John Rhee <funkyx@nate.com>
2020-04-05 00:01:14 +0900

    js-constructor

--- js/constructor.js
+++ js/constructor.js
@@ -1,7 +1,9 @@
 const obj1 = () => {
     class whatever {
         constructor (name) {
-            this.name = name
+            this.name = name,
+            this.f1 = function() {return 'I am ' + name},
+            this.f2 = function() {return name.toUpperCase()}
         }
     }
     return whatever;
@@ -10,4 +12,7 @@ const obj1 = () => {
 const obj2 = obj1();
 let person1 = new obj2('Brad');
 
-console.log(person1.name);
+// console.log(person1.name);
+
+// console.log(person1.f1());
+console.log(person1.f2());
