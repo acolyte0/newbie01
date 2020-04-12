cf93bb6c4a1f51f770b7f002636af063b4632244
John Rhee <funkyx@nate.com>
2020-04-12 15:57:34 +0900

    js math random

---
+++ js/math_random.js
@@ -0,0 +1,4 @@
+let v1 = Math.floor(Math.random() * 10) ;// returns a random integer from 0 to 9
+let v2 = Math.floor(Math.random() * 11); // returns a random integer from 0 to 10
+let v3 = Math.floor(Math.random() * 100) ;// returns a random integer from 0 to 99
+console.log(v1, v2, v3);
