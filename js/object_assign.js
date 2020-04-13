4d5136660a70791903025c7b0a6b9475137a6708
John Rhee <funkyx@nate.com>
2020-04-14 08:59:57 +0900

    js object assign

---
+++ js/object_assign.js
@@ -0,0 +1,7 @@
+let a1 = ['apple','lemon', 'mango', 'orange', 'pineapple'];
+let o1 = {title: 'heading 1', path: 'www.none.com', content:'paragraph 1', arr1: ''}
+
+// Object.assign(o1.array1, a1); // 1st argument must exist
+
+Object.assign(o1.arr1, a1);
+console.log(o1);
