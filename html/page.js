626dc55c0fcf261dbaf8ab14067fb8633e7c9071
John Rhee <funkyx@nate.com>
2020-04-23 01:13:38 +0900

    html dom

--- html/page.js
+++ html/page.js
@@ -32,8 +32,25 @@ let pTitle = document.createElement('title');
 let tTxt = document.createTextNode('The newbie\'s page');
 pTitle.appendChild(tTxt);
 pHead.appendChild(pTitle);
+let cssLink = document.createElement('link');
+cssLink.setAttribute('rel', 'stylesheet');
+cssLink.setAttribute('href', 'style.css');
+pHead.appendChild(cssLink);
+
 let pBody = document.getElementsByTagName('body')[0];
 let bTitleH1 = document.createElement('h1');
 let bTH1Txt = document.createTextNode('This is a heading 1');
 bTitleH1.appendChild(bTH1Txt);
 pBody.appendChild(bTitleH1);
+
+let ul01 = Array(4).fill(null);
+let ul = document.createElement('ul');
+for (let  i = 0; i < ul01.length;i++) {
+    ul01[i] = document.createElement('li');
+    ul01[i].appendChild(document.createTextNode('List string ' + i));
+    ul.appendChild(ul01[i]);
+}
+Object.assign(ul.style, {
+    listStyle: 'none'
+});
+pBody.appendChild(ul);
