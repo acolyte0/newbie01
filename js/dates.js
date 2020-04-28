e531cd891d13072ad1d4c3dc6c9144b958b3a0a5
John Rhee <funkyx@nate.com>
2020-04-28 09:51:24 +0900

    js dates

--- js/dates.js
+++ js/dates.js
@@ -18,15 +18,15 @@ function getNumberOfDaysOfMonth(today) {
     today.setSeconds(0);
     today.setMilliseconds(0);
     console.log('line 20: ' + today);
-    console.log(today.getMonth());
+    console.log('month of today: ' + today.getMonth());
     let tV1 = today.getTime();
-    console.log('line 23: ' + tV1);
+    console.log('get time of the month: ' + tV1);
     let nextMonth = new Date(tV1); // must create another object
     nextMonth.setMonth(today.getMonth() + 1);
     console.log(today);
     console.log(nextMonth);
     let nV1 = nextMonth.getTime();
-    console.log((nV1 - tV1) / 86400000);
+    console.log('# of days of this month is ' + (nV1 - tV1) / 86400000);
 }
 
 let t1 = Date.now();
@@ -35,3 +35,7 @@ let t2 = new Date(t1);
 getNumberOfDaysOfMonth(t2);
 
 // https://www.w3schools.com/js/js_date_methods.asp
+
+let t3 = new Date();
+let t4 = t3.toLocaleTimeString();// convert to local time zone.
+console.log(t4);
