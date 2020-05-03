946404e6e5ddbf54b339afee53f277702b648c30
John Rhee <funkyx@nate.com>
2020-05-03 10:52:10 +0900

    js hexadecimal

---
+++ js/hexadecimal.js
@@ -0,0 +1,8 @@
+let v1 = 15;
+let v1hex = v1.toString(16);
+console.log('hexadecimal value: ' + v1hex);
+console.log(parseInt(v1hex, 16));
+let v2 = Math.random();
+console.log(v2);
+let v2hex = v2.toString(16);
+console.log(v2hex);
