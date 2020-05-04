7e0ffe7907774fd669b316247c25a9d0870589d9
John Rhee <funkyx@nate.com>
2020-05-05 05:48:00 +0900

    js ddm

--- drop_down_menu/ddm.js
+++ drop_down_menu/ddm.js
@@ -26,6 +26,38 @@ let heading1 = cE(
     }
 );
 
+let menuBlock01 = cE(
+    'div',
+    {
+        display: 'block',
+        backgroundColor: '#cc9900'
+    }
+)
+
+let ul01 = cE(
+    'ul',
+    {
+        display: 'table',
+        backgroundColor: 'gray',
+        margin: '0 auto',
+        listStyle: 'none'
+    }
+)
+
+let li01 = cE(
+    'li',
+    {
+        display: 'block',
+        padding: '8px',
+        backgroundColor: '#997300',
+        color: 'white'
+    }
+)
+
+li01.appendChild(cTN('list 01'));
+ul01.appendChild(li01);
+menuBlock01.appendChild(ul01);
+
 heading1.appendChild(cTN('Heading 1'));
 
-pasteEs(heading1);
+pasteEs(heading1, menuBlock01);
